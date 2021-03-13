95fea89a0604c01b0d5a8e0f463e98deb0289aa2
Bruno Arroyo <alu0101123677@ull.edu.es>
2021-03-13 17:05:27 +0000

    inicializacion

--- src/ejercicio-1.ts
+++ src/ejercicio-1.ts
@@ -1,3 +1,9 @@
+/**
+ * Función que suma dos valores
+ * @param valor1 tipo number con el primer valor
+ * @param valor2 tipo number con el segundo valor
+ * @returns retorna un valor suma de valor1 y valor2
+ */
 export function add(valor1: number, valor2:number): number {
  
   return valor1 + valor2;
