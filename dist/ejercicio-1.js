57f3875942a23bddeb2d842d6f864b88680f4ed2
Bruno Arroyo <alu0101123677@ull.edu.es>
2021-03-13 16:38:23 +0000

    inicializacion

---
+++ dist/ejercicio-1.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.add = void 0;
+function add(valor1, valor2) {
+    return valor1 + valor2;
+}
+exports.add = add;
+console.log(add(5, 6));
---
+++ src/ejercicio-1.ts
@@ -0,0 +1,6 @@
+export function add(valor1: number, valor2:number): number {
+ 
+  return valor1 + valor2;
+}
+
+console.log(add(5, 6));
