6eb7275c6fb3adf9dde3d5f4a636d1243b78bc1e
Bruno Arroyo <alu0101123677@ull.edu.es>
2021-03-17 11:33:01 +0000

    ejercicio-PE101 ver1.4

--- src/ejercicio-PE101/class_asignature.ts
+++ src/ejercicio-PE101/class_asignature.ts
@@ -9,9 +9,18 @@ class Asignature {
   setName(name: string): void{
     this.name = name;
   }
+
+  getTeacherName() {
+    return this.teacher.getName();
+  }
+
+  getStudentName() {
+    return this.student.getName();
+  }
   
   classroom(){
-
+    console.log(`La asignatura ${this.getName()} tiene el siguiente profesor: ${this.getTeacherName()}
+    y el almuno es ${this.getStudentName()} `);
   }
 
   listStudent(){
