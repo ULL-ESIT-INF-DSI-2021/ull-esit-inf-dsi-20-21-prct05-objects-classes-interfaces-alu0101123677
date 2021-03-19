3ea2440ba03b1774effe4e17a59d9b945048f5e5
Bruno Arroyo <alu0101123677@ull.edu.es>
2021-03-19 19:10:03 +0000

    ejercicio1 1.0

--- tests/ejercicio-1.spec.ts
+++
@@ -1,9 +0,0 @@
-import 'chai';
-import {expect} from 'chai';
-import {add} from '../src/ejercicio-1';
-
-describe('add function tests', () => {
-  it('add(1, 8) returns value 9', () => {
-    expect(add(1, 8)).to.be.equal(9);
-  })
-})
