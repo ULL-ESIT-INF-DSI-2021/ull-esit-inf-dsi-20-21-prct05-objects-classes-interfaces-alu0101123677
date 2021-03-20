c6626e11f6e7990dd3d9e8c0538aa842d6dbadf8
Bruno Arroyo <alu0101123677@ull.edu.es>
2021-03-20 16:54:59 +0000

    ejercicio2 1.1

--- src/ejercicio-2/class_article.ts
+++ src/ejercicio-2/class_article.ts
@@ -1,11 +1,11 @@
-class Article {
+export class Article {
   constructor(public title: string, public autor: string[], public email: string[], 
-    public keywords: string[], public abstrac: string, public date: string, public editorial: string,
-    public appointments: number){
+    public keywords: string[], public abstrac: string, public date: Date, public editorial: string,
+    public citations: number){
 
     }
    
   APAFormat() {
-  
+    console.log(`${this.autor}. (${this.date.getFullYear()}). ${this.title}. ${this.editorial}.`)
   }  
 }
