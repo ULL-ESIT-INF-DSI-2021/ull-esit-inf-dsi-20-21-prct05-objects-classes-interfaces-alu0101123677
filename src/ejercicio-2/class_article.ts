7fafd3763a0255a979382be7201fdf041c58019d
Bruno Arroyo <alu0101123677@ull.edu.es>
2021-03-20 15:24:53 +0000

    ejercicio2 1.0

---
+++ src/ejercicio-2/class_article.ts
@@ -0,0 +1,11 @@
+class Article {
+  constructor(public title: string, public autor: string[], public email: string[], 
+    public keywords: string[], public abstrac: string, public date: string, public editorial: string,
+    public appointments: number){
+
+    }
+   
+  APAFormat() {
+  
+  }  
+}
