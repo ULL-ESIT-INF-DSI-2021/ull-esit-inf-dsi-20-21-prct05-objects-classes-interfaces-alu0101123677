abb9764a1b9edf5db18f9e4341780d68c1fd52e1
Bruno Arroyo <alu0101123677@ull.edu.es>
2021-03-20 12:04:12 +0000

    ejercicio1 1.5

--- src/ejercicio-1/class_combat.ts
+++ src/ejercicio-1/class_combat.ts
@@ -6,7 +6,39 @@ export class Combat {
       
   start() {
     console.log("El COMBATE TERMINA");
-    console.log(`Los combatientes son: ${this.pokemon1.getName()} y ${this.pokemon2.getName()}`) 
+    console.log(`Los combatientes son: ${this.pokemon1.getName()} y ${this.pokemon2.getName()}`);
+    let healt_zero: boolean = false;
+    while (healt_zero == false) {
+      let first: number = 1;
+      let second: number = 2;
+      if (this.pokemon1.getSpeed() <= this.pokemon2.getSpeed()) {
+        first = 2;
+        second = 1;
+        console.log(`El pokemon ${this.pokemon2.getName()} ataca primero`);
+      }
+      else {
+        console.log(`El pokemon ${this.pokemon1.getName()} ataca primero`);
+      }
+
+      if (first == 1) {
+       let damage: number = this.damage(this.pokemon1.getType(), this.pokemon2.getType(), this.pokemon1.getAttack(), this.pokemon2.getDefense());
+       console.log(`El pokemon ${this.pokemon1.getName()} hace ${damage} puntos de daño`);
+      }
+      else {
+        let damage: number = this.damage(this.pokemon2.getType(), this.pokemon1.getType(), this.pokemon2.getAttack(), this.pokemon1.getDefense());
+        console.log(`${this.pokemon2.getName()} hace ${damage} puntos de daño`);
+      }
+
+      if (second == 2) {
+        let damage: number = this.damage(this.pokemon2.getType(), this.pokemon1.getType(), this.pokemon2.getAttack(), this.pokemon1.getDefense());
+        console.log(`El pokemon ${this.pokemon2.getName()} hace ${damage} puntos de daño`);
+      }
+      else {
+        let damage: number = this.damage(this.pokemon1.getType(), this.pokemon2.getType(), this.pokemon1.getAttack(), this.pokemon2.getDefense());
+        console.log(`${this.pokemon1.getName()} hace ${damage} puntos de daño`);
+      }
+      healt_zero = true;
+    }
   }
 
   damage(type: string, type_oponent: string, attack: number, defense: number): number {
