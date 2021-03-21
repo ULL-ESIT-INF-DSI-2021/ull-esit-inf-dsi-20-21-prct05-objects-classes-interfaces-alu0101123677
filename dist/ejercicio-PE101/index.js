62a03f0ca15fcd39bcf3aa9cb2e1d72e05e5a2c2
Bruno Lorenzo Arroyo Pedraza <72444930+alu0101123677@users.noreply.github.com>
2021-03-21 20:45:43 +0000

    Delete dist directory

--- dist/ejercicio-PE101/index.js
+++
@@ -1,4 +0,0 @@
-let bruno = new Student("bruno", "arroyo", "02/03/1999", "male", "alu0101123677@gmail.com");
-let lucas = new Teacher("lucas", "gonzalo", "03/02/1980", "male", "alu0348190291@ull.edu.es");
-let DSI = new Asignature("dsi", lucas, bruno);
-DSI.classroom();
