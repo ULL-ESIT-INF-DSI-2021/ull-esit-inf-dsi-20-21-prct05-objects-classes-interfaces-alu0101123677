d74466473482d260cc159643beb1add078decced
Bruno Arroyo <alu0101123677@ull.edu.es>
2021-03-21 20:34:59 +0000

    prueba unitaria ejercicio 2 1.1

--- tests/ejercicio-2/index.spec.ts
+++ tests/ejercicio-2/index.spec.ts
@@ -3,7 +3,7 @@ import {expect} from 'chai';
 import {main2} from '../../src/ejercicio-2/index';
 
 describe('Función main que inicializa los objetos de la clases Article y Gestors', () => {
-  it('main("Sudeep") returns value "Sudeep D. Thepade" ', () => {
-    expect(main2("Suddep")).to.be.equal("Sudeep D. Thepade")
+  it('main2("Sudeep") returns value "Sudeep D. Thepade" ', () => {
+    expect(main2("Sudeep")).to.be.eql("Sudeep D. Thepade")
   });
 });
